227f1b343ec1e589f4d05a889267db0ba1f46eef
matiasnc <mn.calderon97@gmail.com>
2026-09-26 13:42:55 -0300

    #2 quito carpeta backend y normalizo raiz

--- app/etl/pipeline.py
+++ app/etl/pipeline.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from app.etl.extractor import ExcelExtractor
-from app.etl.transformer import SIUTransformer
+from app.etl.transformer import SIUTransformer # (O ExcelTransformer según como lo hayas llamado)
+from app.config import config  # Importamos el archivo que creamos arriba
 
 def ejecutar_etl_base():
-    # Ubicamos el archivo en la carpeta 'files' en la raíz del proyecto
-    ruta_excel = Path(__file__).resolve().parent.parent.parent / "files" / "estadisticas_fin_cursada.xlsx"
+    # Variable de configuración para armar la ruta
+    ruta_excel = Path(__file__).resolve().parent.parent.parent / "files" / config.EXCEL_SIU_FILENAME
     
     try:
         # 1. Extracción
@@ -15,13 +16,12 @@ def ejecutar_etl_base():
         transformer = SIUTransformer()
         df_limpio = transformer.limpiar_basico(df_crudo)
         
-        # 3. Carga (Temporal: Impresión por consola según el Ticket)
+        # 3. Carga
         print("\n--- Primeras 5 filas limpias ---")
         print(df_limpio.head())
-        print(f"\n[INFO] Total de registros listos para la base de datos: {len(df_limpio)}")
         
     except Exception as e:
-        print(f"[ERROR CRÍTICO] Falló la ejecución del pipeline: {e}")
+        print(f"Error en el pipeline: {e}")
 
 if __name__ == "__main__":
     ejecutar_etl_base()
